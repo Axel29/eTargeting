bad38cfc866a5906edd9635b1093d75fd04bbb5b
Axel29 <axel.bouaziz@hotmail.fr>
2014-04-28 00:05:02 +0200

    Commit test

--- src/eTargeting/Index.java
+++ src/eTargeting/Index.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Index")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
